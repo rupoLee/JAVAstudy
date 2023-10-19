9008a6b51d1b1b9634f8d41cfb948e374d922559
ljw <ljw01080@gmail.com>
2023-10-19 15:08:55 +0900

    타입완료

--- JAVAstudy/src/ex01/O2_type.java
+++ JAVAstudy/src/ex01/O2_type.java
@@ -44,7 +44,7 @@ public class O2_type {
 //		System.out.println(c6);
 //		System.out.println(uniCode);
 		
-		
+		//정수
 //		int var1 = 10;	//10진수
 //		int var2 = 012;	//8진수
 //		int var3 = 0xA;	//16진수
@@ -64,11 +64,35 @@ public class O2_type {
 //		System.out.println(var4);
 		
 		
+		//실수
+//		double var1 = 3.14;
+//		float var2 = 3.14f;		//float는 f나 F 추가
+//		System.out.println("var1 : "+var1);
+//		System.out.println("var2 : "+var2);
+//		
+//		//정밀도 테스트
+//		double var4 = 0.1234567890123456789;
+//		float var5 = 0.1234567890123456789f;
+//		System.out.println("var4 : "+var4);
+//		System.out.println("var5 : "+var5);
+//		
+//		//e사용
+//		int var6 = 3000000;
+//		double var7 = 3e6;	//e=10의 지수, 실수에서 저장
+//		float var8 = 3e6f;
+//		double var9 = 2e-3;
+//		System.out.println("var6 : "+var6);
+//		System.out.println("var7 : "+var7);
+//		System.out.println("var8 : "+var8);
+//		System.out.println("var9 : "+var9);
 		
 		
-		
-		
-		
+//		boolean stop = true;
+//		if(stop) {
+//			System.out.println("중지합니다.");
+//		} else {
+//			System.out.println("시작합니다.");
+//		}
 	}
 
 }
