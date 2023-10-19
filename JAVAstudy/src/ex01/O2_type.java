c780a2729f7f8793f125376219d4c44b44b7d23e
ljw <ljw01080@gmail.com>
2023-10-19 14:03:57 +0900

    char, int, long

--- JAVAstudy/src/ex01/O2_type.java
+++ JAVAstudy/src/ex01/O2_type.java
@@ -25,6 +25,50 @@ public class O2_type {
 //		}
 		
 		
+//		char c = ''; //에러// > char c = ' ';
+//		char c1 = 'A';		//문자
+//		char c2 = 65;		//10진수
+//		char c3 = '\u0041';	//16진수
+//		
+//		char c4 = '가';		//문자
+//		char c5 = 44032;	//10진수
+//		char c6 = '\uac00';	//16진수
+//		
+//		int uniCode = c1;	//유니코드 확인
+//		
+//		System.out.println(c1);
+//		System.out.println(c2);
+//		System.out.println(c3);
+//		System.out.println(c4);
+//		System.out.println(c5);
+//		System.out.println(c6);
+//		System.out.println(uniCode);
+		
+		
+//		int var1 = 10;	//10진수
+//		int var2 = 012;	//8진수
+//		int var3 = 0xA;	//16진수
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+//		System.out.println(var3);
+		
+		
+//		long var1 = 10;
+//		long var2 = 20L;			//초기화 시 l이나 L 추가
+////		long var3 = 1000000000000;	//int 저장범위 넘으면 에러
+//		long var4 = 1000000000000L;	//l이나 L 추가하면 에러 안남
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+//		System.out.println(var4);
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
