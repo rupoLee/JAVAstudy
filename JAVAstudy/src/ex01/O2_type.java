6d258db014c701bcaae62de91ce402fe2fbf983a
ljw <ljw01080@gmail.com>
2023-10-19 13:26:31 +0900

    byte 완료

--- JAVAstudy/src/ex01/O2_type.java
+++ JAVAstudy/src/ex01/O2_type.java
@@ -3,18 +3,28 @@ package ex01;
 public class O2_type {
 
 	public static void main(String[] args) {
-		byte var1 = -128;
-		byte var2 = -30;
-		byte var3 = 0;
-		byte var4 = 30;
-		byte var5 = 127;
-//		byte var6 = 128; //Type mismatch: cannot convert from int to byte
+//		byte var1 = -128;
+//		byte var2 = -30;
+//		byte var3 = 0;
+//		byte var4 = 30;
+//		byte var5 = 127;
+////		byte var6 = 128; //Type mismatch: cannot convert from int to byte
+//		
+//		System.out.println(var1);
+//		System.out.println(var2);
+//		System.out.println(var3);
+//		System.out.println(var4);
+//		System.out.println(var5);
+//		
+//		byte var = 125;
+//		int var0 = 125;
+//		for (int i=0; i<5; i++) {
+//			var++;
+//			var0++;
+//			System.out.println("var : "+var+"\t"+"var0 : "+var0);
+//		}
 		
-		System.out.println(var1);
-		System.out.println(var2);
-		System.out.println(var3);
-		System.out.println(var4);
-		System.out.println(var5);
+		
 	}
 
 }
