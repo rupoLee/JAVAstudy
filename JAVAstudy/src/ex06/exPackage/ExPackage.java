da91924db81a31d73f9276b46d0e83fad4ae8af8
ljw <ljw01080@gmail.com>
2023-10-27 17:57:05 +0900

    package

---
+++ JAVAstudy/src/ex06/exPackage/ExPackage.java
@@ -0,0 +1,11 @@
+package ex06.exPackage;
+
+public class ExPackage {
+
+	public static void main(String[] args) {
+		
+		//상위 패키지와 하위 패키지를 .으로 구분 ex)상위.하위
+		System.out.println("애플리케이션 실행");
+	}
+
+}
