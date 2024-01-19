7259cee6aedbb7c63069b60d410e54d5cc8af6bf
ljw <ljw01080@gmail.com>
2024-01-19 11:26:20 +0900

    주석 처리

--- JAVAstudy/src/ex07_Inheritance/O5_2_SportCar.java
+++ JAVAstudy/src/ex07_Inheritance/O5_2_SportCar.java
@@ -7,10 +7,12 @@ public class O5_2_SportCar extends O5_1_Car {
 		speed += 10;
 	}
 	
+	/*
 	//오버라이딩 할 수 없음
 	@Override
 	public void stop() {
 		System.out.println("스포츠카를 멈춤");
 		speed = 0;
 	}
+	*/
 }
